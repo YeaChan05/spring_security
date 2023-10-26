7c549438ece378cfdc771953dbe7c09c86605fde
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2023-10-26 19:31:54 +0900

    filterChain 여러개 등록

--- src/main/java/org/yeachan/spring_security/config/SecurityConfig.java
+++ src/main/java/org/yeachan/spring_security/config/SecurityConfig.java
@@ -2,8 +2,6 @@ package org.yeachan.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,30 +9,27 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
     @Bean
-    @Order(Ordered.LOWEST_PRECEDENCE - 100)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .securityMatcher("/")// 해당 url로 요청 할 때 적용되는 filter 설정
-                .authorizeHttpRequests(registry -> registry//http 요청에 대한 인가 설정
-                        .requestMatchers("/", "/info", "/account/**").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .anyRequest().authenticated())
+        http.authorizeHttpRequests(registry ->//http 요청에 대한 인가 설정
+                        registry.requestMatchers("/", "/info","/account/**").permitAll()
+                                .requestMatchers("/admin").hasRole("ADMIN")
+                                .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
 
-    @Bean
-    @Order(Ordered.LOWEST_PRECEDENCE - 15)
-    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
-        http
-                .securityMatcher("/account/**")
-                .authorizeHttpRequests(registry -> registry
-                        .anyRequest().permitAll())
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
-
-        return http.build();
-    }
+//    @Bean
+//    @Order(Ordered.LOWEST_PRECEDENCE - 15)
+//    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
+//        http
+//                .securityMatcher("/account/**")
+//                .authorizeHttpRequests(registry -> registry
+//                        .anyRequest().permitAll())
+//                .formLogin(Customizer.withDefaults())
+//                .httpBasic(Customizer.withDefaults());
+//
+//        return http.build();
+//    }
 }
