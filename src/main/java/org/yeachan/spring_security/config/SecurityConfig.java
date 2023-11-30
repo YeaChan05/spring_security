b4fc6da84728b57536ccfbe8a1ee68742795885b
YeaChan05 <qkenrdl05@gmail.com>
2023-12-01 00:11:58 +0900

    authorizationDecision을 사용한 권한 결정과 roleHierarchy을 사용한 권한 계층 지정

--- src/main/java/org/yeachan/spring_security/config/SecurityConfig.java
+++ src/main/java/org/yeachan/spring_security/config/SecurityConfig.java
@@ -2,34 +2,55 @@ package org.yeachan.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+
+import java.util.function.Supplier;
 
 @Configuration
 public class SecurityConfig {
     @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return roleHierarchy;
+    }
+    @Bean
+    public SecurityExpressionHandler<FilterInvocation> webSecurityExpressionHandler() {
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy());
+        return handler;
+    }
+
+        @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(registry ->//http 요청에 대한 인가 설정
                         registry.requestMatchers("/", "/info","/account/**").permitAll()
-                                .requestMatchers("/admin").hasRole("ADMIN")
-                                .anyRequest().authenticated())
+                                .requestMatchers("/admin").access(this::authorizationDecision)
+                                .requestMatchers("/user").hasRole("USER")
+                                .anyRequest().authenticated()
+                )
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
 
-//    @Bean
-//    @Order(Ordered.LOWEST_PRECEDENCE - 15)
-//    public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
-//        http
-//                .securityMatcher("/account/**")
-//                .authorizeHttpRequests(registry -> registry
-//                        .anyRequest().permitAll())
-//                .formLogin(Customizer.withDefaults())
-//                .httpBasic(Customizer.withDefaults());
-//
-//        return http.build();
-//    }
+    private AuthorizationDecision authorizationDecision(Supplier<Authentication> authenticationSupplier, RequestAuthorizationContext requestAuthorizationContext) {
+        if (authenticationSupplier.get().getAuthorities().stream()
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("ROLE_ADMIN"))) {
+            return new AuthorizationDecision(true);
+        }
+        return new AuthorizationDecision(false);
+    }
+
 }
